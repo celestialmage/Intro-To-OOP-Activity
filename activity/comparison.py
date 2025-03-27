b6a2774ad59705dd89dd10442990c057cf1ea64e
Max <imax.mr@gmail.com>
2025-03-27 11:02:08 -0700

    Completed Activity

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,10 @@
 # add your get_student_with_more_classes function here!
+def get_student_with_more_classes(student_1, student_2):
+    result = None
+
+    if student_1.get_num_classes() > student_2.get_num_classes():
+        result = student_1
+    else:
+        result = student_2
+    
+    return result
--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,15 @@
 # add your Student class here!
+class Student:
+    def __init__ (self, name: str, year: str, classes: list):
+        self.name = name
+        self.year = year
+        self.classes = classes
+
+    def add_class(self, subject):
+        self.classes.append(subject)
+
+    def get_num_classes(self):
+        return len(self.classes)
+    
+    def summary(self):
+        return f"{self.name} is a {self.year} with {self.get_num_classes()} classes"
